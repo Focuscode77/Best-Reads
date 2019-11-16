af316dd9c116fa8f4944fcbf49a0710099e6db1d
Amjad Shaaban <theamjad@outlook.com>
2019-11-16 13:17:02 -0500

    Google sign in test html page and route

--- server.js
+++ server.js
@@ -8,16 +8,15 @@ var app = express();
 app.use(express.static("public"));
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-
-var routes = require();
-
-app.use(routes);
+require("./routes/html-routes")(app);
 
 
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
