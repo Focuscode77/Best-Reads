a76bafbeeb5328353401eaf9ce4abd4b4ea1b7bc
Joshua Coleman <procoleman77@outlook.com>
2019-11-18 18:44:37 -0500

    Added db files and connect to server

--- models/book.js
+++ models/book.js
@@ -1,10 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
     var Todo = sequelize.define("BestReads", {
-      book: DataTypes.STRING,
-      complete: DataTypes.BOOLEAN
+       Title: DataTypes.STRING,
+       Author:DataTypes.STRING,
+       Rating:DataTypes.Number,
+       Cover:DataTypes.STRING,
+
     });
 
 
     
     return BestReads;
   };
+
+  
+    
+   
