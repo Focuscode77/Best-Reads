1fc30015a8a6b5556c6d93ba84b609d513dd5653
Amjad Shaaban <theamjad@outlook.com>
2019-11-17 21:52:30 -0500

    added node-fetch, we have xml conversion to json

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 var passport = require("../config/passport");
+var xmlConvert = require("xml-js");
+var fetch = require("node-fetch");
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
@@ -8,29 +10,64 @@ module.exports = function (app) {
     })
   );
 
-  app.get("/logout", function (req, res) {
+  app.get("/logout", function(req, res) {
     req.logout();
     req.session.destroy();
     res.redirect("/");
   });
 
-  app.get("/user", function (req, res) {
+  app.get("/user", function(req, res) {
     if (req.session.passport) {
       res.json(req.session.passport.user.displayName);
     } else {
-      res.json(false)
+      res.json(false);
     }
   });
+  app.get("/xmltest2", function(req, res) {
+    var queryURL =
+      "https://www.goodreads.com/search/index.xml?key=ntj35uAln93Ca74x0mChdA&q=Madeline";
 
+    fetch(queryURL)
+      .then(response => response.text())
+      .then(data => {
+        // data = the raw xml data
+        // console.log(data);
+        var compactJson = xmlConvert.xml2json(data, {
+          compact: true,
+          spaces: 4
+        });
+        console.log(compactJson);
+        res.send(compactJson);
+      });
+  });
+
+  app.get("/xmltest", function(req, res) {
+    var queryURL =
+      "https://www.goodreads.com/search/index.xml?key=ntj35uAln93Ca74x0mChdA&q=Madeline";
+
+    fetch(queryURL)
+      .then(response => response.text())
+      .then(data => {
+        // data = the raw xml data
+        // console.log(data);
+
+        var fullJson = xmlConvert.xml2json(data, {
+          compact: false,
+          spaces: 4
+        });
+        console.log(fullJson);
+        res.send(fullJson);
+      });
+  });
 
   app.get(
     "/auth/google/callback",
     passport.authenticate("google", {
       failureRedirect: "/"
     }),
-    function (req, res) {
+    function(req, res) {
       // Successful authentication, redirect home.
-      res.redirect("/");
+      res.redirect("/test");
       // res.json(req.user.displayName)
     }
   );
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var session = require("express-session");
 var passport = require("./config/passport");
-
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -11,7 +10,12 @@ app.use(express.static("public"));
 //handlebars - initialize handlebars and create routes
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
 app.use(
@@ -21,7 +25,11 @@ app.use(
 );
 app.use(express.json());
 app.use(
-  session({ secret: "BestReads", resave: true, saveUninitialized: true })
+  session({
+    secret: "BestReads",
+    resave: true,
+    saveUninitialized: true
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
