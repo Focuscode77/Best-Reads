1c64df37e8030f8d7747d08e30688e3b19888c1e
Amjad Shaaban <theamjad@outlook.com>
2019-11-19 14:31:37 -0500

    Changes: * Amazon login fixed. * HTML routes point to main instead of test

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    "operatorsAliases": false
+  development: {
+    username: "root",
+    password: "",
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
   },
-  "test": {
-    "username": "root",
-    "password": "",
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: "root",
+    password: "",
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
   },
-  "production": {
-    "username": "root",
-    "password": "",
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    operatorsAliases: false
   }
 };
--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,17 +1,27 @@
 $(document).ready(function (req, res) {
-    var displayBody = $(".display")
-    var linksBody = $(".linkz")
-    var loginLink = $("#login")
+    var displayBody = $("#display")
+    var linksBody = $("#linkz")
+    var loginGoogle = $("#login-google")
+    var loginAmazon = $("#login-amazon")
+
     var logoutLink = $("#logout")
     logoutLink.detach();
+    // console.log("Yo i ran made them vars!");
     // loginLink.on("click", () => {
     $.get("/user").then(function (data) {
         if (data) {
-            displayBody.text("Welcome: " + data);
-            loginLink.detach();
+            console.log(data);
+            var userObject = displayBody.text("Welcome: " + data.name + "\nLogged in thru: " + data.provider);
+
+            userObject.html(userObject.html().replace(/\n/g, '<br/>'));
+            loginGoogle.detach();
+            loginAmazon.detach();
             logoutLink.appendTo(linksBody);
+        } else {
+            console.log(false);
         }
     });
+
     //     loginLink.detach();
     //     logoutLink.appendTo(linksBody);
 
