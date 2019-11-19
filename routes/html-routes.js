a1ba19526621085f722905b53d8b818eaa60b1ae
Amjad Shaaban <theamjad@outlook.com>
2019-11-18 20:03:32 -0500

    SORRY <3

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     });
 
     app.get('/', function (req, res, next) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.render('index');
     });
 
     app.get('/home', function (req, res, next) {
