6602eb5ac7bfb46d5eebea2e97e16020bfde750c
maiada <maiadacx@gmail.com>
2019-11-18 20:12:29 -0500

    these are minor changes that can be deleted

--- models/book.js
+++ models/book.js
@@ -1,17 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
-    var Todo = sequelize.define("BestReads", {
-       Title: DataTypes.STRING,
-       Author:DataTypes.STRING,
-       Rating:DataTypes.Number,
-       Cover:DataTypes.STRING,
+    var BestReads = sequelize.define("BestReads", {
+        Title: DataTypes.STRING,
+        Author: DataTypes.STRING,
+        Rating: DataTypes.INTEGER,
+        Cover: DataTypes.STRING,
 
     });
 
 
-    
-    return BestReads;
-  };
 
-  
-    
-   
+    return BestReads;
+};
