fba055337ddd9bb9269043ee9bb680eb4004d6a9
Amjad Shaaban <theamjad@outlook.com>
2019-11-20 20:14:59 -0500

    last push

--- config/config.js
+++ config/config.js
@@ -19,10 +19,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    use_env_variable: JAWSDB_URL,
     dialect: "mysql"
   }
 
