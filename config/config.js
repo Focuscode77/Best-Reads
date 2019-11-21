5ff18aaf60a03d1dbd5c21b279af2ca121222ee3
Amjad Shaaban <theamjad@outlook.com>
2019-11-20 19:13:11 -0500

    heroku db fix

--- config/config.js
+++ config/config.js
@@ -1,22 +1,21 @@
-
 require('dotenv').config();
 
 
 
 module.exports = {
   development: {
-    username: "root",
-    password: "",
-    database: "database_development",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     operatorsAliases: false
   },
   test: {
-    username: "root",
-    password: "",
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     operatorsAliases: false
   },
