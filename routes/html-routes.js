605019cc688bb5ccb7f5446bdc6f0dd5a8643c1a
Amjad Shaaban <theamjad@outlook.com>
2019-11-20 19:40:43 -0500

    heroku setup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,45 +1,37 @@
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
     // app.get("*", function(req, res) {
     //     res.sendFile(path.join(__dirname, "../public/test.html"));
     // });
-<<<<<<< HEAD
-    app.get("/test", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/test.html"));
-    });
-=======
     // app.get("/test", function (req, res) {
     //     res.sendFile(path.join(__dirname, "../public/test.html"));
     // });
->>>>>>> master
 
-    app.get('/', function(req, res, next) {
+    app.get('/', function (req, res, next) {
         res.render('index');
     });
 
-<<<<<<< HEAD
-    app.get('/home', function(req, res, next) {
+    app.get('/home', function (req, res, next) {
         res.render('pages/home');
     });
 
-    app.get('/current-book', function(req, res, next) {
+    app.get('/current-book', function (req, res, next) {
         res.render('pages/current-book');
     });
 
-    app.get('/advanced', function(req, res, next) {
+    app.get('/advanced', function (req, res, next) {
         res.render('pages/advanced-search');
     });
 
-    app.get('/mylists', function(req, res, next) {
-        res.render('pages/my-lists');
-    });
+    // app.get('/mylists', function(req, res, next) {
+    //     res.render('pages/my-lists');
+    // });
 
     // app.get('/profile', function (req, res, next) {
     //     res.render('pages/profile');
     // });
 
-=======
     app.get('/mylists', function (req, res, next) {
         let checker = false;
         app.get("/user", function (req, res, next) {
@@ -69,5 +61,4 @@ module.exports = function(app) {
     //     res.render('pages/profile');
     // });
 
->>>>>>> master
 };
