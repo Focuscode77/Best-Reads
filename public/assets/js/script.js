257c6a174a165f526290e4b888e98018f2a1d2ba
maiada <maiadacx@gmail.com>
2019-11-25 20:25:51 -0500

    fixed profile html route

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,14 @@
-//anytime a button with the class current-book-link is clicked on the site, 
-//the user will be taken to the book profile with a custom url
+$(document).ready(function() {
 
-$(document).on("click", ".current-book-link", function() {
-    var id = $(this).attr('id')
-    var title = $(this).attr('data-title');
-    var author = $(this).attr('data-author');
-    var image = $(this).attr('data-img');
-    var rating = $(this).attr('data-rating');
-    location.replace("/currentbook?book=" + encodeURI(title) + "&author=" + encodeURI(author) + "&image=" + image + "&rating=" + rating + "&id=" + id);
+    //anytime a button with the class current-book-link is clicked on the site, 
+    //the user will be taken to the book profile with a custom url
+    $(document).on("click", ".current-book-link", function() {
+        var id = $(this).attr('id')
+        var title = $(this).attr('data-title');
+        var author = $(this).attr('data-author');
+        var image = $(this).attr('data-img');
+        var rating = $(this).attr('data-rating');
+        location.replace("/currentbook?book=" + encodeURI(title) + "&author=" + encodeURI(author) + "&image=" + image + "&rating=" + rating + "&id=" + id);
+    });
+
 });
