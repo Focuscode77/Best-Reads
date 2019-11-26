f8a13b41487f08882f60c871097e33e7d1d0760b
Amjad Shaaban <theamjad@outlook.com>
2019-11-25 20:10:18 -0500

    users DB ready

--- models/reads_list_items.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-    var reads_lists = sequelize.define("reads_list_items", {
-        user_id: DataTypes.INTEGER
-    });
-    return reads_lists;
-};
--- server.js
+++ server.js
@@ -15,31 +15,31 @@ var exphbs = require("express-handlebars");
 var Handlebars = require("handlebars");
 
 app.engine(
-    "handlebars",
-    exphbs({
-        defaultLayout: "main"
-    })
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
 );
 app.set("view engine", "handlebars");
 
 app.use(
-    express.urlencoded({
-        extended: true
-    })
+  express.urlencoded({
+    extended: true
+  })
 );
 app.use(express.json());
 app.use(
-    cookieSession({
-        name: "BestReads",
-        keys: [
-            "484af53583463faaf38e407eccb84107",
-            "ea9d53b7d78ffebcc03416aaad4685c1",
-            "b0d3a8c8cde1091f271a64b857beb0c6",
-            "1f75f56f493e4641bbe3c59d4c8d4d64",
-            "6c3600607c7566ac7c1a1e5e7e2f22a3"
-        ],
-        maxAge: 24 * 60 * 60 * 1000
-    })
+  cookieSession({
+    name: "BestReads",
+    keys: [
+      "484af53583463faaf38e407eccb84107",
+      "ea9d53b7d78ffebcc03416aaad4685c1",
+      "b0d3a8c8cde1091f271a64b857beb0c6",
+      "1f75f56f493e4641bbe3c59d4c8d4d64",
+      "6c3600607c7566ac7c1a1e5e7e2f22a3"
+    ],
+    maxAge: 24 * 60 * 60 * 1000
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -51,11 +51,11 @@ require("./routes/post-api-routes")(app);
 // Hi
 console.log("OMG!");
 db.sequelize
-    .sync({
-        force: true
-    })
-    .then(function() {
-        app.listen(PORT, function() {
-            console.log("App listening on PORT " + PORT);
-        });
+  .sync({
+    force: true
+  })
+  .then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
     });
+  });
