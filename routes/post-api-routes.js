a255e5ad09117cb3c1beebe8f53053a1a951a116
maiada <maiadacx@gmail.com>
2019-11-27 20:29:01 -0500

    post works to add book to list from search results

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -1,10 +1,10 @@
 var db = require("../models");
 
 
-module.exports = function (app) {
+module.exports = function(app) {
 
 
-    app.get("/api", function (req, res) {
+    app.get("/api", function(req, res) {
         var query = {};
         if (req.query.author_id) {
             query.AuthorId = req.query.author_id;
@@ -13,55 +13,44 @@ module.exports = function (app) {
         db.Post.findAll({
             where: query,
             include: [db.Author]
-        }).then(function (dbPost) {
+        }).then(function(dbPost) {
             res.json(dbPost);
         });
     });
 
 
-    app.get("/api/add", function (req, res) {
+    app.get("/api/add", function(req, res) {
 
         db.Post.findOne({
             where: {
                 id: req.params.id
             },
             include: [db.Author]
-        }).then(function (dbPost) {
+        }).then(function(dbPost) {
             res.json(dbPost);
         });
     });
 
-    // POST route for saving a new post
-    app.post("/api/add/:book/:cat", function (req, res) {
-        db.reads_lists.create({
-            user_id: profile.uid,
-            book_id: req.params.book,
-            cat_id: req.params.cat_id
-
-        }).then(function (dbPost) {
-            res.json("something happened");
-        });
-    });
 
     // DELETE route for deleting posts
-    app.delete("/api/posts/:id", function (req, res) {
+    app.delete("/api/posts/:id", function(req, res) {
         db.Post.destroy({
             where: {
                 id: req.params.id
             }
-        }).then(function (dbPost) {
+        }).then(function(dbPost) {
             res.json(dbPost);
         });
     });
 
     // PUT route for updating posts
-    app.put("/api/posts", function (req, res) {
+    app.put("/api/posts", function(req, res) {
         db.Post.update(
             req.body, {
                 where: {
                     id: req.body.id
                 }
-            }).then(function (dbPost) {
+            }).then(function(dbPost) {
             res.json(dbPost);
         });
     });
