c2dd731b17ff1dfda6baf7c51e0524a8da488e04
maiada <maiadacx@gmail.com>
2019-12-01 17:25:52 -0500

    idk still bork

--- models/users.js
+++ models/users.js
@@ -1,30 +1,13 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
 
     var users = sequelize.define("users", {
-<<<<<<< HEAD
         name: DataTypes.INTEGER,
-        pictureUrl: DataTypes.STRING,  
+        pictureUrl: DataTypes.STRING,
         email: DataTypes.STRING,
         dub: DataTypes.STRING,
         username: DataTypes.STRING
     });
 
     return users;
-    
-};
-
-
-
-
-=======
-        name: DataTypes.STRING,
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        dob: DataTypes.INTEGER,
-        pictureURL: DataTypes.STRING
-    });
-
-    return users;
 
 };
->>>>>>> master
